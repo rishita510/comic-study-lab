fc5692c1c70c8c2699e659d361592419fb516af2
rishita2410 <isroscience2@gmail.com>
2025-11-15 18:54:20 +0530

    added link tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
+
 function Navbar() {
   return (
     <nav className="global-navbar">
   <div className="top-nav">
     <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/page4">Activity</a></li>
-      <li><a href="/page6">Creativity</a></li>
-      <li><a href="/page3">Origin</a></li>
-      <li><a href="/page5">Visibility</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/page4">Activity</Link></li>
+      <li><Link to="/page6">Creativity</Link></li>
+      <li><Link to="/page3">Origin</Link></li>
+      <li><Link to="/page5">Visibility</Link></li>
     </ul>
   </div>
 </nav>
